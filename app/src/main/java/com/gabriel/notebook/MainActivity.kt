b7030cc289819e4849f2cdae5b30baf28081c053
rajadurai_gabriel <rajadurai.gabriel@gmail.com>
2020-02-26 21:28:56 +0530

    Set activity action bar to toolbar view from the layout

--- app/src/main/java/com/gabriel/notebook/MainActivity.kt
+++ app/src/main/java/com/gabriel/notebook/MainActivity.kt
@@ -3,12 +3,14 @@ package com.gabriel.notebook
 import android.os.Bundle
 import com.gabriel.notebook.base.BaseActivity
 import com.gabriel.notebook.base.BaseFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
     }
 
     override fun getCurrentFragment(): BaseFragment? {
