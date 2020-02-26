33b2c8f7f2123eb30974fbc72e0523ef4bfbfcbe
rajadurai_gabriel <rajadurai.gabriel@gmail.com>
2020-02-26 18:32:37 +0530

    Setup two-way databinding to retain content of title and notes fields

--- app/src/main/java/com/gabriel/notebook/features/addNotes/AddNotesViewModel.kt
+++ app/src/main/java/com/gabriel/notebook/features/addNotes/AddNotesViewModel.kt
@@ -2,7 +2,10 @@ package com.gabriel.notebook.features.addNotes
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 
 class AddNotesViewModel(app: Application) : AndroidViewModel(app) {
 
+    val title = MutableLiveData<String>()
+    val notes = MutableLiveData<String>()
 }
