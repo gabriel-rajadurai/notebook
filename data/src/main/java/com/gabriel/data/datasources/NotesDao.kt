55438c464a66b6ded6106d785611e89e131babed
rajadurai_gabriel <rajadurai.gabriel@gmail.com>
2020-02-27 17:33:50 +0530

    Add query to fetch all Notes from DB, in descending order of createdAt

--- data/src/main/java/com/gabriel/data/datasources/NotesDao.kt
+++ data/src/main/java/com/gabriel/data/datasources/NotesDao.kt
@@ -1,8 +1,10 @@
 package com.gabriel.data.datasources
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.gabriel.data.models.Note
 
 @Dao
@@ -10,4 +12,7 @@ interface NotesDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveNote(note: Note)
+
+    @Query("Select * from notes_table ORDER BY createdAt DESC")
+    fun getAllNotes(): LiveData<List<Note>>
 }
