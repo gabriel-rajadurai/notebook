fb806c6e8e0021f199a8b430d09804ed0c98eac4
rajadurai_gabriel <rajadurai.gabriel@gmail.com>
2020-02-27 16:32:12 +0530

    Add Room Entity for Note

---
+++ data/src/main/java/com/gabriel/data/models/Note.kt
@@ -0,0 +1,12 @@
+package com.gabriel.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes_table")
+class Note(
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    var title: String,
+    var content: String,
+    var createdAt: String
+)
