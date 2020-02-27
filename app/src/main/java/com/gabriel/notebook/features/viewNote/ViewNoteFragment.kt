33b3dc6f043e7efe234c84059f971dfc1b7f6395
rajadurai_gabriel <rajadurai.gabriel@gmail.com>
2020-02-27 21:54:14 +0530

    Implement logic for fetching and showing Note details

--- app/src/main/java/com/gabriel/notebook/features/viewNote/ViewNoteFragment.kt
+++ app/src/main/java/com/gabriel/notebook/features/viewNote/ViewNoteFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 
 import com.gabriel.notebook.R
 import com.gabriel.notebook.base.BaseFragment
 import com.gabriel.notebook.common.ViewModelFactory
 import com.gabriel.notebook.common.setActionBarTitle
 import com.gabriel.notebook.common.showBackButton
+import kotlinx.android.synthetic.main.view_note_fragment.*
+import kotlinx.coroutines.launch
 
 class ViewNoteFragment : BaseFragment() {
 
@@ -30,7 +33,18 @@ class ViewNoteFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setActionBarTitle("") //TODO Set title to Notes title
+        arguments?.getInt(BUN_NOTE_ID)?.let {
+            lifecycleScope.launch {
+                viewNoteViewModel.getNoteById(it)?.let { note ->
+                    setActionBarTitle(note.title)
+                    tvNote.text = note.content
+                }
+            }
+        } ?: throw IllegalStateException("Note ID cannot be null")
         showBackButton()
+    }
+
+    companion object {
+        const val BUN_NOTE_ID = "bunNoteId"
     }
 }
--- app/src/main/java/com/gabriel/notebook/features/viewNote/ViewNoteViewModel.kt
+++ app/src/main/java/com/gabriel/notebook/features/viewNote/ViewNoteViewModel.kt
@@ -2,7 +2,11 @@ package com.gabriel.notebook.features.viewNote
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.gabriel.data.repositories.NotesRepository
 
-class ViewNoteViewModel(app:Application) : AndroidViewModel(app) {
+class ViewNoteViewModel(app: Application) : AndroidViewModel(app) {
 
+    private val notesRepository by lazy { NotesRepository(getApplication()) }
+
+    suspend fun getNoteById(id: Int) = notesRepository.getNoteById(id)
 }
