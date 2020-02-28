6055d2ffb4df9177b1eb68fb51bab2ebec038e7f
rajadurai_gabriel <rajadurai.gabriel@gmail.com>
2020-02-28 14:27:37 +0530

    Add extension method to show Snackbar

--- app/src/main/java/com/gabriel/notebook/common/Extensions.kt
+++ app/src/main/java/com/gabriel/notebook/common/Extensions.kt
@@ -1,10 +1,12 @@
 package com.gabriel.notebook.common
 
 import android.app.Application
+import android.view.View
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.AndroidViewModel
+import com.google.android.material.snackbar.Snackbar
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -45,3 +47,7 @@ fun Date.toFormat(format: String): String {
 
 fun AndroidViewModel.getString(@StringRes resId: Int) =
     getApplication<Application>().getString(resId)
+
+fun View.showSnackBar(msg: String) {
+    Snackbar.make(this, msg, Snackbar.LENGTH_SHORT).show()
+}
