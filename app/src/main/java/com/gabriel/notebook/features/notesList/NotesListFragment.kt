12bd3f031f5ed3e9b75cc53ba946fdbf640f95c6
Gabi <rajadurai.gabriel@gmail.com>
2020-07-06 11:49:41 +0530

    Add option to add selected text as a Note

--- app/src/main/java/com/gabriel/notebook/features/notesList/NotesListFragment.kt
+++ app/src/main/java/com/gabriel/notebook/features/notesList/NotesListFragment.kt
@@ -1,5 +1,8 @@
 package com.gabriel.notebook.features.notesList
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +20,7 @@ import com.gabriel.notebook.common.SpacesItemDecoration
 import com.gabriel.notebook.common.ViewModelFactory
 import com.gabriel.notebook.common.hideBackButton
 import com.gabriel.notebook.common.setActionBarTitle
+import com.gabriel.notebook.features.addNotes.AddNotesFragment
 import com.gabriel.notebook.features.viewNote.ViewNoteFragment
 import kotlinx.android.synthetic.main.notes_list_fragment.*
 import javax.inject.Inject
@@ -31,6 +35,18 @@ class NotesListFragment : BaseFragment(), NotesListAdapter.NotesItemClickListene
     override fun onCreate(savedInstanceState: Bundle?) {
         (requireContext().applicationContext as NotesApplication).appGraph.inject(this)
         super.onCreate(savedInstanceState)
+        //Handling ACTION_PROCESS_TEXT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requireActivity().intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)?.let {
+                if (it.isNotBlank()) {
+                    findNavController().navigate(R.id.addNotesFragment, Bundle().apply {
+                        putString(AddNotesFragment.BUN_ACTION, Intent.EXTRA_PROCESS_TEXT)
+                        putString(AddNotesFragment.NOTE_PROCESS_TEXT, it.toString())
+                    })
+                }
+            }
+        }
+        retainInstance = true
     }
 
     override fun onCreateView(
